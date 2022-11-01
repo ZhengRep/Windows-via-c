b5c4d0796c0b62bccc329d70ff38a9665e11d947
ZhengLi <2726401288@qq.com>
2022-11-01 11:35:24 +0800

    to print log

---
+++ CreateObject/Source.cpp
@@ -0,0 +1,21 @@
+#include<windows.h>
+/*
+Create:
+CreateMutex()
+CreateEvent()
+CreateSemaphore()
+CreateWaitableTimer()
+CreateFileMapping()
+CreateJobObject()
+
+dwErrorCode = GetLastError();
+
+Open:
+OpenMutex()
+OpenEvent()
+OpenSamaphore()
+OpenWaitableTimer()
+OpenFileMapping()
+OpenJobObjec()
+
+*/
---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# Windows via c
+
+该项目是书籍《windows via c/c++》（）的学习项目
+
+## 项目
+
+### ERRORSHOW
+
+该项目
